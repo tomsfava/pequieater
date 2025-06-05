28c29608c68997df2f38c39a3d6b973da04604f0
Tomás Fava <tomsfava@gmail.com>
2025-06-05 20:12:14 -0300

    removendo importações desnecessárias

--- post/views.py
+++ post/views.py
@@ -1,7 +1,5 @@
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 
