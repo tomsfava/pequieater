5af534edd345c4bf9171c5a6e4def8c50b6c0f98
Tomás Fava <tomsfava@gmail.com>
2025-06-05 15:35:34 -0300

    criando user detail html e view

--- core/views.py
+++ core/views.py
@@ -3,10 +3,18 @@ from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
-from user.forms import UserRegisterForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpResponseForbidden
+from user.forms import UserRegisterForm, UserUpdateForm
+
+User = get_user_model()
 
 class LoginView(DjangoLoginView):
     template_name = "login.html"
+
+    def get_success_url(self):
+        return reverse_lazy("user-detail", kwargs={"pk": self.request.user.pk})
 
 class RegisterView(FormView):
     template_name = "register.html"
@@ -16,3 +24,39 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+class UserDetailTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = "user_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile_user = get_object_or_404(User, pk=self.kwargs["pk"])
+        current_user = self.request.user
+        is_own_profile = profile_user == current_user
+
+        form = UserUpdateForm(instance=current_user) if is_own_profile else None
+        following = profile_user.following.all()
+
+        context.update({
+            "profile_user": profile_user,
+            "is_own_profile": is_own_profile,
+            "form": form,
+            "following": following,
+        })
+        return context
+
+    def get(self, request, *args, **kwargs):
+        profile_user = get_object_or_404(User, pk=self.kwargs["pk"])
+        current_user = request.user
+        is_own_profile = profile_user == current_user
+
+        edit_mode = request.GET.get("edit") == "true"
+
+        if edit_mode:
+            if not is_own_profile:
+                return HttpResponseForbidden("Você não pode editar este perfil.")
+            form = UserUpdateForm(instance=current_user)
+            html = render_to_string("partials/_user_update_form.html", {"form": form}, request=request)
+            return HttpResponse(html)
+
+        return super().get(request, *args, **kwargs)
