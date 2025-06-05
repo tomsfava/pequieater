f80f7e058f2c5489d6c2c62634f0e61ae6990bbc
Tomás Fava <tomsfava@gmail.com>
2025-06-05 08:52:41 -0300

    config crispy-forms

--- backend/settings.py
+++ backend/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "core",
     "user",
 ]
@@ -71,6 +73,8 @@ TEMPLATES = [
         },
     },
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "backend.wsgi.application"
 
